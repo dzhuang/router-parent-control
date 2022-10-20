50615ca6d13d00be7f1dcb65212401b381522df0
dzhuang <dzhuang.scut@gmail.com>
2022-10-20 15:33:15 +0800

    Enable gunicorn log.

--- parent_control/start-server.sh
+++ parent_control/start-server.sh
@@ -5,5 +5,5 @@ python manage.py migrate --noinput
 
 python manage.py createsuperuser --no-input
 
-(gunicorn parent_control.wsgi --user pc_user --bind 0.0.0.0:8010 --workers 3) &
+(gunicorn parent_control.wsgi --user pc_user --bind 0.0.0.0:8010 --access-logfile /opt/parent_control/log/access_file_g.log --error-logfile /opt/parent_control/log/error_file_g.log --capture-output --enable-stdio-inheritance --workers 3) &
 sudo nginx
