b5ebcbd46d5db6e10b0b128d5be5a9dfe3d7aac8
dzhuang <dzhuang.scut@gmail.com>
2023-05-30 16:33:39 +0800

    Allow reboot router via task.

--- parent_control/my_router/tasks.py
+++ parent_control/my_router/tasks.py
@@ -1,10 +1,16 @@
 from django.utils.translation import gettext as _
 
 from celery import shared_task
-from my_router.views import fetch_new_info_save_and_set_cache
+from my_router.views import do_reboot_router, fetch_new_info_save_and_set_cache
 
 
 @shared_task(bind=True, name="fetch_devices_and_set_cache")
 def fetch_devices_and_set_cache(self, router_id):
     fetch_new_info_save_and_set_cache(router_id)
+    return {"message": _("Done")}
+
+
+@shared_task(bind=True, name="reboot_router")
+def reboot_router(self, router_id):
+    do_reboot_router(router_id)
     return {"message": _("Done")}
